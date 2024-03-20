eed8516dbaffdf033a3a275513941747c243b75c
Avery Cheng <AveryZC14@gmail.com>
2024-03-21 02:00:41 +1100

    Partitioning works!

--- partitioning.py
+++ partitioning.py
@@ -48,20 +48,24 @@ def hoare_partition(lst,p = None):
     lst[0],lst[right] = lst[right],lst[0]
     return lst
 
-def dutch_flag_partition(lst,p = None):
+def dutch_flag_partition(lst,p = None,low = None,high = None):
+    # if len(lst) <= 1:
+    #     return lst,None,None
+    low = low if low else 0
+    high = high if high else len(lst)-1
     if p == None:
         # low,mid,high = (lst[0],lst[len(lst)//2],lst[-1])
         # p = (low if low > mid and low < high else (mid if low < mid else high))
         
         # low,mid,high = (0,len(lst)//2,len(lst)-1)
         # p = (low if (lst[low] > lst[mid] and lst[low] < lst[high]) or (lst[low] > lst[high] and lst[low] < lst[mid]) else (mid if lst[low] < lst[mid] else high))
-        cands = [(0,lst[0]),(len(lst)//2,lst[len(lst)//2]),(len(lst)-1,lst[len(lst)-1])]
+        cands = [(low,lst[low]),((low+high+1)//2,lst[(low+high+1)//2]),(high,lst[high])]
         cands.sort(key=(lambda i:i[1]))
         p = cands[1][0]
     p_val = lst[p]
-    blue = white = 0
+    blue = white = low
     # white = 0
-    red = len(lst)-1
+    red = high
     while white <= red:
         if lst[white] < p_val:
             lst[white],lst[blue] = lst[blue],lst[white]
@@ -72,16 +76,23 @@ def dutch_flag_partition(lst,p = None):
             red -= 1
         else:
             white += 1
-    return lst
+    return (lst,blue,white)
         
 def quick_sort(lst,low = None, high = None,p = None):
+    
     if low == None:
         low = 0
     if high == None:
         high = len(lst)-1
-    if high <= low:
-        return
-    dutch_flag_partition(lst,)
+    if low >= high:
+        return lst
+    # if high <= low:
+    #     return
+    lst,blue,white = dutch_flag_partition(lst,None,low,high)
+    quick_sort(lst,low,blue-1)
+    quick_sort(lst,white,high)
+    return lst
+    
     
     
     
@@ -95,5 +106,8 @@ def quick_sort(lst,low = None, high = None,p = None):
 if __name__ == "__main__":
     lst1 = [1,8,2,6,4,8,4,12,32,14,18,43]
     lst2 = [12,9,2,8,3,7,4,6,5,5,1,7]
-    print(dutch_flag_partition(lst1))
-    plot(dutch_flag_partition(lst1))
+    lst3 = [99,12,82,28,83,48,85,68,75,38,94,27,39,4,93,27,46,53,12,22,1,44,44,44,44]
+    # print(dutch_flag_partition(lst1))
+    # plot(dutch_flag_partition(lst1))
+    print(quick_sort(lst3))
+    plot(quick_sort(lst3))
